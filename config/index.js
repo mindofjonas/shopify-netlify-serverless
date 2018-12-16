88144fd6a3607f02bc40c371abea834fae8f6711
Jonas Fleur-Aime <hello@mindofjonas.com>
2018-12-16 03:46:29 -0500

    Add app secret to config

--- config/index.js
+++ config/index.js
@@ -10,5 +10,6 @@ export default {
   authorizePath: `${shopifyApiBaseUrl}/admin/oauth/authorize`,
   tokenPath: `${shopifyApiBaseUrl}/admin/oauth/access_token`,
   redirect_uri: `${process.env.APP_URL}/.netlify/functions/shopify-auth-callback`,
-  appUrl: process.env.APP_URL
+  appUrl: process.env.APP_URL,
+  appSecret: process.env.APP_SECRET
 };
